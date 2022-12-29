30121ced1489a2083473cc74782abcb5e8741852
Made Suka <121066706+madesuka@users.noreply.github.com>
2022-12-29 20:02:12 +0700

    Update donasi_app.py

--- donasi_app.py
+++ donasi_app.py
@@ -31,7 +31,7 @@ img_lottie_animation = Image.open("images/rekkrema1.jpeg")
 # ---- HEADER SECTION ----
 with st.container():
     st.subheader("Ayo kita berdonasi secara online ! :wave:")
-    st.title("Sumbangan Sukarela Pembangunan Krematorium dan Rumah Duka")
+    st.title("Sumbangan Sukarela Pembangunan Krematorium dan Rumah Duka Desa Adat Banyuasri Singaraja Bali")
     st.write(
         " Donasi dapat dikirimkan kepada Nomor Rekening Bank Mandiri KCP Singaraja 145-00- 1310040-5 Panitia Pembangunan Kreamtorium dan Rumah Duka"
     )
