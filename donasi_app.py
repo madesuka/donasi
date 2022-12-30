8d1229b2f2a4e7b7c9e5812f10337de180192297
Made Suka <121066706+madesuka@users.noreply.github.com>
2022-12-30 18:15:16 +0700

    Update donasi_app.py

--- donasi_app.py
+++ donasi_app.py
@@ -25,7 +25,7 @@ local_css("style/style.css")
 
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
-img_contact_form = Image.open("images/atang.jpeg")
+img_contact_form = Image.open("images/situasi.jpeg")
 img_lottie_animation = Image.open("images/rekkrema1.jpeg")
 
 # ---- HEADER SECTION ----
