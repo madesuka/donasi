6536d966de7d5852dbf18b9ffa70e9c3207f7bb8
Made Suka <121066706+madesuka@users.noreply.github.com>
2022-12-30 18:23:58 +0700

    Update donasi_app.py

--- donasi_app.py
+++ donasi_app.py
@@ -74,7 +74,7 @@ with st.container():
             Bertujuan :
             1. Menyediakan fasilitas umum dalam prosesi adat masyarakat hindu atau umat lain yg melakukan proses pembakaran jenazah
             2. Memfasilitasi kebutuhan prosesi adat yg telah memenuhi syarat legal baik skala maupun niskala
-            3. Membantu masyarakat menyediakan sarana untuk dapat melakukan prosesi adat secara efisien dan terukur sesuai pedoman adat
+            3. Membantu masyarakat menyediakan sarana untuk dapat melakukan prosesi adat secara efisien dan terukur sesuai sastra agama
             4. Mendorong kemajuan masyarakat dalam melakukan upacara adat sesuai dengan perkembangan jaman
             """
         )
